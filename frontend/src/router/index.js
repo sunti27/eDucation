1f3ffbbc07cb6d1b9d91e7bfc423af9c7bc90542
Fabian Wassermann <fabian.wassermann04@gmail.com>
2022-12-20 16:11:57 +0100

    classroom route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Overview from "@/views/Overview.vue";
+import Classroom from "@/views/Classroom.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/overview",
       name: "overview",
       component: Overview,
+    },
+    {
+      path: "/classroom",
+      name: "classroom",
+      component: Classroom,
     },
   ],
 });
